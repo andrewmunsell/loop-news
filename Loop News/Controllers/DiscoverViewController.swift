8a6403e15d8855846a250c4c6a72257c000b09fd
Andrew Munsell <andrew@wizardapps.net>
2015-12-03 19:07:54 -0800

    Add menu buttons for the discover screen. Fixes #10.

--- Loop News/Controllers/DiscoverViewController.swift
+++ Loop News/Controllers/DiscoverViewController.swift
@@ -7,5 +7,19 @@
 //
 
 class DiscoverViewController: UIViewController {
+    @IBOutlet weak var titleBar: UINavigationItem!
+
+    override func viewDidLoad() {
+        self.titleBar.leftBarButtonItem = UIBarButtonItem(title: "Map", style: .Plain, target: self, action: "showMap:")
+        
+        self.titleBar.rightBarButtonItem = UIBarButtonItem(title: "Settings", style: .Plain, target: self, action: "showSettings:")
+    }
     
+    func showMap(sender: AnyObject?) {
+        self.performSegueWithIdentifier("ShowMap", sender: sender)
+    }
+    
+    func showSettings(sender: AnyObject?) {
+        self.performSegueWithIdentifier("ShowSettings", sender: sender)
+    }
 }
