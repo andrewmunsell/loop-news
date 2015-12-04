6ef7494b1b7f0dffbabacde37c4aa2133d2ba40a
Andrew Munsell <andrew@wizardapps.net>
2015-12-03 17:58:54 -0800

    Add search results screen template and view controller

---
+++ Loop News/Controllers/SearchResultsViewController.swift
@@ -0,0 +1,11 @@
+//
+//  SearchResultsViewController.swift
+//  Loop News
+//
+//  Created by Andrew Munsell on 12/3/15.
+//  Copyright © 2015 Loop News. All rights reserved.
+//
+
+class SearchResultsViewController: UIViewController {
+    
+}
