c9813007b8efc02647e63ab00debd01365edf546
Andrew Munsell <andrew@wizardapps.net>
2015-12-03 17:57:34 -0800

    Add skeleton view controllers for Discover, Timeline, and Settings

---
+++ Loop News/Controllers/DiscoverViewController.swift
@@ -0,0 +1,11 @@
+//
+//  DiscoverViewController.swift
+//  Loop News
+//
+//  Created by Andrew Munsell on 12/3/15.
+//  Copyright © 2015 Loop News. All rights reserved.
+//
+
+class DiscoverViewController: UIViewController {
+    
+}
