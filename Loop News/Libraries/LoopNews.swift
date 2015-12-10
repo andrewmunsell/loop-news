b9ca768d4f1451e0d742979a9143014180881f7a
Andrew Munsell <andrew@wizardapps.net>
2015-12-09 18:16:19 -0800

    Add basic implementation of the news lib

--- Loop News/Libraries/LoopNews.swift
+++ Loop News/Libraries/LoopNews.swift
@@ -6,12 +6,28 @@
 //  Copyright © 2015 Loop News. All rights reserved.
 //
 
+import Parse
+
 class LoopNews {
+    /**
+     * Get all of the events
+     */
     static func getAllEvents(callback: ([Event]?, NSError?) -> Void) {
+        let query = Event.query()!
         
+        query.findObjectsInBackgroundWithBlock { (events: [PFObject]?, err: NSError?) -> Void in
+            callback(events as? [Event], err)
+        }
     }
     
-    static func getEvent(callback: (Event?, NSError?) -> Void) {
+    /**
+     * Get an event by its ID
+     */
+    static func getEvent(event: String, callback: (Event?, NSError?) -> Void) {
+        let query = Event.query()!
         
+        query.getObjectInBackgroundWithId(event) { (event: PFObject?, err: NSError?) -> Void in
+            callback(event as? Event, err)
+        }
     }
 }
