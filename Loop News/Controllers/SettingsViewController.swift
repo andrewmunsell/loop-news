377e0315c7922b0cbd7cd4a9794402bf835278aa
Andrew Munsell <andrew@wizardapps.net>
2015-12-09 18:56:10 -0800

    Implement initial settings view w/ subscription list and refresh

--- Loop News/Controllers/SettingsViewController.swift
+++ Loop News/Controllers/SettingsViewController.swift
@@ -6,6 +6,52 @@
 //  Copyright © 2015 Loop News. All rights reserved.
 //
 
-class SettingsViewController: UIViewController {
+import Eureka
+
+class SettingsViewController: FormViewController {
+    private var subsSection: Section?
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Setup the navigation bar
+        self.navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .Refresh, target: self, action: "refresh")
+        
+        // Setup the form
+        self.subsSection = Section("Followed Events")
+        
+        self.form +++ self.subsSection!
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        self.refresh()
+    }
+    
+    /**
+     * Refresh the user's subscriptions
+     */
+    func refresh() {
+        self.subsSection?.removeAll(keepCapacity: true)
+        
+        Subscription.forCurrentUser { (subs: [Subscription]?, err: NSError?) -> Void in
+            if subs != nil {
+                for sub in subs! {
+                    self.subsSection!
+                        <<< LabelRow() {
+                            $0.title = sub.event.title
+                        }
+                }
+            }
+            
+            if subs == nil || subs!.count < 1 {
+                self.subsSection!
+                    <<< LabelRow() {
+                        $0.title = "You don't have any subscriptions."
+                        $0.disabled = true
+                        
+                        $0.evaluateDisabled()
+                    }
+            }
+        }
+    }
 }
