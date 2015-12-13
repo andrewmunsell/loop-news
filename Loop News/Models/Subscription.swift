7289ec2170cfb481295021a4cdef45a1734390d6
Andrew Munsell <andrew@wizardapps.net>
2015-12-13 12:20:17 -0800

    Add methods to find subs by user and event

--- Loop News/Models/Subscription.swift
+++ Loop News/Models/Subscription.swift
@@ -39,6 +39,14 @@ class Subscription: PFObject, PFSubclassing {
     }
     
     /**
+     * Get the current user's subscription for the specified event, or nil if they are
+     * not subscribed to the event
+     */
+    static func forCurrentUserAndEvent(event: Event, callback: (Subscription?, NSError?) -> Void) {
+        Subscription.forUser(PFUser.currentUser()!, andEvent: event, callback: callback)
+    }
+    
+    /**
      * Get the subscriptions for the specified user
      */
     static func forUser(user: PFUser, callback: ([Subscription]?, NSError?) -> Void) {
@@ -51,6 +59,23 @@ class Subscription: PFObject, PFSubclassing {
         
         query.findObjectsInBackgroundWithBlock { (subs: [PFObject]?, err: NSError?) -> Void in
             callback(subs as? [Subscription], err)
+        }
+    }
+    
+    /**
+     * Get the subscription for the specified user and event, or nil if the user is not subscribed
+     * to the specified event.
+     */
+    static func forUser(user: PFUser, andEvent event: Event, callback: (Subscription?, NSError?) -> Void) {
+        let query = Subscription.query()!
+        
+        query.whereKey("user", equalTo: user)
+        query.whereKey("event", equalTo: event)
+        
+        query.cachePolicy = .NetworkElseCache
+        
+        query.findObjectsInBackgroundWithBlock { (subs: [PFObject]?, err: NSError?) -> Void in
+            callback(subs?[0] as? Subscription, err)
         }
     }
 }
